441dd39968d1c7eb93737df339ebd617063f199b
nguyenquocdat30 <boelovenhan@gmail.com>
2017-05-30 01:17:37 +0700

    Load image

--- src/gameMario/Game.java
+++ src/gameMario/Game.java
@@ -1,5 +1,14 @@
 package gameMario;
+import java.awt.*;
+import javax.swing.*;
 
-public class Game {
-
+public class Game extends JFrame{
+	public Game()
+	{
+		setTitle("Game Mario - Team DCH");
+		setSize(1600,1000);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		add(new LoadImage());
+		setVisible(true);
+	}
 }
---
+++ src/gameMario/Main.java
@@ -0,0 +1,9 @@
+package gameMario;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Game app = new Game();
+	}
+
+}
